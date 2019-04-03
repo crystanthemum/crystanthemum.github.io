d0850a9dce4063498e9e7b3113e78ba66c85d1d2
crystanthemum <37309905+crystanthemum@users.noreply.github.com>
2019-04-03 19:32:28 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Crystal Fong</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="mycss.css">
+        <link href="https://fonts.googleapis.com/css?family=Reem+Kufi" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+    <body>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+       
+        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #FFA500;">
+            <a class="navbar-brand" href="index.php">Crystal Fong</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="education.php">Education</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="workexp.html">Work Experience</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="volunteer.html">Volunteer</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.linkedin.com/in/crystal-fong">LinkedIn</a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="https://github.com/crystanthemum">GitHub</a>
+                    </li>
+                </ul>
+             
+                
+            
+                </ul>
+            </div>
+        </nav>
+
+    <div class="container-fluid">
+        <div class="row" >
+            <div class="col-2"></div>
+            <div class="col smallpad">
+                <h1>
+                    Hello this is Crystal :
+                    <br>
+                    software developer 
+                </h1>
+                <br>
+
+                <a href="images/crystal_fong_cv.pdf" download>
+                <p>download my CV</p>           
+                </a>
+            </div>
+            
+            <div class="col ">              
+                    <img src="images/xmas_markets.jpg" class="img-fluid mymargin" alt="head shot">
+            </div>
+            <div class="col-2"></div>
+        </div>
+        <div class="row" id="aboutme" style="background-color: yellow">
+
+            <div class="col-1">
+            </div>
+            
+            <div class="col-2 mypadd">
+                <br>
+                <h2>About me</h2>
+            </div>
+            <div class="col">
+                <p class="mypadd">             
+                With a background in psychology I am currently challenging myself by undertaking a MSc conversion course in software development. 
+                Utilising my psychology degree, I believe I have a balanced skillset to be a successful software developer. 
+                Outside of university, attending a wide range of tech-related Meetups allows me to explore the scope of the IT industry and meet influential people within the industry. I have always been involved in a team environment whether it be academic or from my part-time jobs, working with others has always been second nature to me. Although I have only started web development in January for semester two, 
+                applying aesthetics on top of logical problem-solving is a combination that particularly appeals to me.
+                </p>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
